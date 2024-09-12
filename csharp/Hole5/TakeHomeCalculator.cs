bf4ec5cfac1a9e93e5a5ebe826f2f99227408712
JuanCruzMedina <juancruzfaq@gmail.com>
2024-09-12 02:18:19 -0300

    refactor: [auto] move to new file

--- csharp/Hole5/TakeHomeCalculator.cs
+++ csharp/Hole5/TakeHomeCalculator.cs
@@ -1,25 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 
 namespace Hole5
 {
-    public class TaxRate
-    {
-        private readonly int percent;
-
-        public TaxRate(int percent)
-        {
-            this.percent = percent;
-        }
-
-        public Money Apply(Money total)
-        {
-            Double amount = total.value * (percent / 100d);
-            return Money.Create(Convert.ToInt32(amount), total.currency);
-        }
-    }
-
     public class TakeHomeCalculator
     {
         private readonly TaxRate taxRate;
